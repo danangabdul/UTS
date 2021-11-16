d660c598d1de4563b7ce7ef6d844ddae03438164
danangabdul <54830216+danangabdul@users.noreply.github.com>
2021-11-16 20:00:06 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Halaman Login</title>
+  </head>
+  <body>
+    <form>
+      <label>Username :</label>
+      <input type ="text" name="fusername"><br>
+      <label>password :</labeel>
+      <input type="password" name="fpassword"><br>
+      <button type="submit" name="fmasuk">login</button>
+    </form>
+  </body>
+  <?html>
